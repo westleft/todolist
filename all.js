112ad1b22569a2128038d144d967f4ec8516a55f
lin <leftwest576@gmail.com>
2021-06-03 22:32:47 +0800

    修正打勾

--- all.js
+++ all.js
@@ -8,27 +8,25 @@ const rest = document.querySelector('.restbox')
 const clear = document.querySelector('.clear')
 
 
-
 let save = document.querySelector('.save');
 let text_content = document.querySelector('.text');
 
-
 save.addEventListener('click', function () {
-    if(text_content.value == ''){        
+    if (text_content.value == '') {
         return;
     }
-    da = {};
-    da.content = text_content.value;
-    da.complete = '待完成';
-    data.push(da);
-    
+    obj = {};
+    obj.content = text_content.value;
+    obj.complete = '待完成';
+    data.push(obj);
+
     rest_item();
     render();
     text_content.value = '';
 })
 
 // 新增代辦事項
-function render() {    
+function render() {
     str = ''
     data.forEach(function (item, index) {
         let con = `
@@ -46,24 +44,27 @@ function render() {
 
 // 點選事件
 main_btn.addEventListener('click', function (e) {
-    console.log(e.target);
+    let paragraph = document.querySelector('.todo');
     let num = e.target.getAttribute('data-num');
+
     // 打勾變成已完成 在勾一次變待完成
-    if (e.target.getAttribute('class') == 'check'){  
+    if (e.target.getAttribute('class') == 'check') {
         if (data[num].complete == '已完成') {
-            data[num].complete = '待完成'; 
-        }else{
-            data[num].complete = '已完成'; 
+            data[num].complete = '待完成';
+            paragraph.style = 'text-decoration:none'
+        } else {
+            data[num].complete = '已完成';
+            paragraph.style = 'text-decoration:line-through'
         }
-    }else if (e.target.getAttribute('class') == 'delete') {
+    } else if (e.target.getAttribute('class') == 'delete') {
         // 刪除事項
         console.log(num);
-        data.splice(num,1);
+        data.splice(num, 1);
         render()
     }
     let strr = '';
-    data.forEach(function(item,index){       
-        if(e.target.value == '全部'){
+    data.forEach(function (item, index) {
+        if (e.target.value == '全部') {
             strr += `<li>
             <label>
               <input type="checkbox" data-num="${index}" class="check" id="${index}">
@@ -71,8 +72,8 @@ main_btn.addEventListener('click', function (e) {
               <input type="button" data-num="${index}"class="delete" value="✕">
             </label>
           </li>`;
-        }       
-        if(e.target.value == item.complete){
+        }
+        if (e.target.value == item.complete) {
             strr += `<li>
             <label>
               <input type="checkbox" data-num="${index}" class="check" id="${index}">
@@ -85,23 +86,30 @@ main_btn.addEventListener('click', function (e) {
     })
     if (e.target.value == '已完成') {
         list.innerHTML = strr
-    }else if (e.target.value == '待完成') {
+    } else if (e.target.value == '待完成') {
         list.innerHTML = strr
-    }else if (e.target.value == '全部') {
+    } else if (e.target.value == '全部') {
         list.innerHTML = strr
     }
-    
+
     rest_item()
 })
 
-
 // 待完成項目
-function rest_item(){
+function rest_item() {
     let x = 0;
-    data.forEach(function(item,index){
-        if(item.complete == '待完成')
-        x += 1
+    data.forEach(function (item, index) {
+        if (item.complete == '待完成')
+            x += 1
     })
     rest.innerHTML = `<p class="rest">${x}個待完成項目</p>`
 }
 
+function decoration(){
+    data.forEach(function(item,index){
+        if(data.index.complete == '待完成'){
+            
+        }
+    })
+        
+}
