8360b7b75bac4053203cc2b0fe3e35958e716686
lin <leftwest576@gmail.com>
2021-06-06 22:22:13 +0800

    fix all function

--- all.js
+++ all.js
@@ -1,200 +1,130 @@
-data = [];
-
-
 let list = document.querySelector('.list');
-let complete_list = document.querySelector('.complete_list');
-let main_btn = document.querySelector('.main_list');
+let main_btn = document.querySelector('.mian_btn');
 const rest = document.querySelector('.restbox')
-const clear = document.querySelector('.clear')
-
 
 let save = document.querySelector('.save');
 let text_content = document.querySelector('.text');
-
-// const list_templete = `
-// <li>
-// <label>
-//   <input type="checkbox" data-num="$(index)" class="check" id="$(index)">
-//   <label class="todo" for="$(index)">$(content)</label>
-//   <input type="button" data-num="$(index)"class="delete" value="✕">
-// </label>
-// </li>`;
+let clear = document.querySelector('.clear')
 
 
+//建立todo清單
+save.addEventListener('click', function(){
+    //建立li,裝所有東西的清單
+    const todo_li = document.createElement('li');
+    todo_li.innerText = '';
 
-save.addEventListener('click', function () {
-    if (text_content.value == '') {
-        return;
-    }
-    obj = {};
-    obj.content = text_content.value;
-    obj.complete = '待完成';
-    data.push(obj);
+    //建立完成按鈕,新增至li
+    const completeButton = document.createElement('input');
+    completeButton.type = 'checkbox'
+    completeButton.innerHTML = '';
+    completeButton.classList.add('check')
+    todo_li.appendChild(completeButton);
 
+    //建立代辦事項,新增至li (文字產生自此)
+    var todoContent = document.createElement('label');
+    todoContent.innerHTML = text_content.value;
+    todoContent.classList.add('todo');
+    todo_li.appendChild(todoContent);
+
+    //建立刪除按鈕,新增至li
+    const deleteButton = document.createElement('button');
+    deleteButton.innerHTML = '✕';
+    deleteButton.classList.add('delete');
+    todo_li.appendChild(deleteButton);
+
+    //新增至畫面
+    list.appendChild(todo_li);
+
+    //清空輸入欄
+    text_content.value = '';
+
+    //導覽列跑回「全部」
+    let li = document.querySelector('li')
     for (var i = 0; i < btn.length; i++) {
         btn[i].classList.remove('btn_active')
     }
     btn[0].classList.add('btn_active')
+    li.style.display = 'flex'
 
-    render();
-    text_content.value = '';
+    //待完成項目
+    rest_item()
 })
 
+//刪除及完成事件
+list.addEventListener('click', function(e){
+    item = e.target;
 
+    //點選刪除按鈕
+    if (item.classList[0] === 'delete') {
+        const todo = item.parentElement;
+        todo.classList.add('delete_animation')
+        // 先執行完CSS才刪除
+        todo.addEventListener('transitionend', function () {
+            todo.remove()
+        })
+    }
 
-
-
-// 新增代辦事項
-function render() {
-    str = ''
-    data.forEach(function (item, index) {
-        if (item.complete == '已完成') {
-            str += `
-            <li>
-            <label>
-              <input type="checkbox" data-num="${index}" class="check" id="${index}" checked>
-              <label class="todo decoration" for="${index}">${item.content}</label>
-              <input type="button" data-num="${index}"class="delete" value="✕">
-            </label>
-            </li>`;
-        }
-        if (item.complete == '待完成') {
-            str += `
-            <li>
-            <label>
-              <input type="checkbox" data-num="${index}" class="check" id="${index}">
-              <label class="todo" for="${index}">${item.content}</label>
-              <input type="button" data-num="${index}"class="delete" value="✕">
-            </label>
-            </li>`
-        }
-    })
-    list.innerHTML = str
+    //點選完成勾勾
+    if (item.classList[0] === 'check') {
+        const todo = item.parentElement;
+        todo.classList.toggle('decoration')
+    }
     rest_item()
-}
+})
 
-// 點選事件
+//篩選全部/已完成/待完成
 main_btn.addEventListener('click', function (e) {
-    let paragraph = document.querySelectorAll('.todo');
-    let num = e.target.getAttribute('data-num');
-    console.log(e.target)
-    // 打勾變成已完成 在勾一次變待完成
-    if (e.target.getAttribute('class') === 'check') {
-        if (data[num].complete === '已完成') {
-            data[num].complete = '待完成';
-            paragraph[num].style = 'text-decoration:none'
-        } else {
-            data[num].complete = '已完成';
-            paragraph[num].style = 'text-decoration:line-through'
-        }
-        rest_item()
-        return;
-    }
-    if (e.target.getAttribute('class') == 'delete') {
-        // 刪除事項
-        console.log(num);
-        data.splice(num, 1);
-        rest_item()
-        render();
-        return;
-    } else if (e.target.getAttribute('class') == 'delete_notyet') {
-        data.splice(num, 1);
-        str = ''
-        data.forEach(function (item, index) {
-            if (data[index].complete == '待完成') {
-                str += `
-            <li>
-            <label>
-              <input type="checkbox" data-num="${index}" class="check" id="${index}">
-              <label class="todo" for="${index}">${item.content}</label>
-              <input type="button" data-num="${index}"class="delete_notyet" value="✕">
-            </label>
-            </li>`;
-            }
+    const todos = list.childNodes;
+    let li = document.querySelector('li')
 
-        }
-        )
-        // // 刪除事項
-
-        list.innerHTML = str
-        rest_item()
-
-        return;
-
-    }
-
-    let strr = '';
-    data.forEach(function (item, index) {
-        // replace_content= list_templete.replaceAll('$(index)', index).replace('$(content)',item.content);
+    //篩選全部/已完成/待完成
+    todos.forEach(function (li) {
         if (e.target.value == '全部') {
-            if (item.complete == '已完成') {
-                strr += `
-                <li>
-                <label>
-                  <input type="checkbox" data-num="${index}" class="check" id="${index}" checked>
-                  <label class="todo decoration" for="${index}">${item.content}</label>
-                  <input type="button" data-num="${index}"class="delete" value="✕">
-                </label>
-                </li>`;
+            li.style.display = 'flex'
+        } else if (e.target.value == '已完成') {
+            if (li.classList.contains('decoration')) {
+                li.style.display = 'flex';
+            } else {
+                li.style.display = 'none';
             }
-            if (item.complete == '待完成')
-                strr += `
-                <li>
-                <label>
-                  <input type="checkbox" data-num="${index}" class="check" id="${index}">
-                  <label class="todo" for="${index}">${item.content}</label>
-                  <input type="button" data-num="${index}"class="delete" value="✕">
-                </label>
-                </li>`;
+        } else if (e.target.value == '待完成') {
+            if (li.classList.contains('decoration')) {
+                li.style.display = 'none';
+            } else {
+                li.style.display = 'flex';
+            }
         }
-        if (e.target.value == item.complete) {
-            if (e.target.value == '已完成') {
-                strr += `
-            <li>
-            <label>
-              <input type="checkbox" data-num="${index}" class="check" id="${index}" checked>
-              <label class="todo decoration" for="${index}">${item.content}</label>
-              <input type="button" data-num="${index}"class="delete" value="✕">
-            </label>
-            </li>`;
-                paragraph.style = 'text-decoration:line-through';;
-            } else if (e.target.value == '待完成') {
-                strr += `
-            <li>
-            <label>
-              <input type="checkbox" data-num="${index}" class="check" id="${index}">
-              <label class="todo" for="${index}">${item.content}</label>
-              <input type="button" data-num="${index}"class="delete_notyet" value="✕">
-            </label>
-            </li>`;
 
-            }
-            render()
-        }
     })
-    if (e.target.value == '已完成') {
-        list.innerHTML = strr
-    } else if (e.target.value == '待完成') {
-        list.innerHTML = strr
-    } else if (e.target.value == '全部') {
-        list.innerHTML = strr
-    }
-
     rest_item()
 })
 
-// 待完成項目
-function rest_item() {
-    let x = 0;
-    data.forEach(function (item, index) {
-        if (item.complete == '待完成')
-            x += 1
+//清除已完成項目
+clear.addEventListener('click', function (e) {
+    const todos = list.childNodes;
+
+    todos.forEach(function(item,index){
+        if (todos[index].classList.contains('decoration')) {
+            todos[index].style.display = 'none';
+        } 
     })
-    rest.innerHTML = `<p class="rest">${x}個待完成項目</p>`
+})
+
+//計算剩餘項目
+function rest_item() {
+    const todos = list.childNodes;
+
+    let uncompleteCount = todos.length
+    todos.forEach(function (item, index) {
+        if (todos[index].classList.contains('decoration')) {
+            uncompleteCount -= 1
+        }
+    })
+    rest.innerHTML = `<p class="rest">${uncompleteCount}個待完成項目</p>`
 }
 
+//導覽列亮燈
 var btn = document.querySelectorAll('.btn')
-
 for (var i = 0; i < btn.length; i++) {
     btn[i].onclick = function () {
         for (var i = 0; i < btn.length; i++) {
